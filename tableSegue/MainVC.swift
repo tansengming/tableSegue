5962ac5940c541958cb2fc023c6112f14a7ad38d
SengMing Tan <tansengming@gmail.com>
2016-12-27 11:46:57 -0500

    rename VC

---
+++ tableSegue/MainVC.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  tableSegue
+//
+//  Created by better on 12/23/16.
+//  Copyright © 2016 sane men. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+    @IBOutlet var MainTable: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
