b6dd7fa561ad004977f5b8d9cbcc0cd3338d88a0
SengMing Tan <tansengming@gmail.com>
2016-12-27 15:41:39 -0500

    hooks up table view data source and delegate

--- tableSegue/MainVC.swift
+++ tableSegue/MainVC.swift
@@ -8,11 +8,21 @@
 
 import UIKit
 
-class MainVC: UIViewController {
+class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var mainTable: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        mainTable.delegate = self
+        mainTable.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 }
