a2b119297d9dd05aa10c9170e8cea5dc42e5d36a
SengMing Tan <tansengming@gmail.com>
2016-12-27 15:10:21 -0500

    fixes more referencing shit

--- tableSegue/MainVC.swift
+++ tableSegue/MainVC.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class MainVC: UIViewController {
+    @IBOutlet weak var mainTable: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
